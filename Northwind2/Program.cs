fb3c45b6e62898617de49f81345cc07d48f677de
Ngwaka <HpxB5rSvsSsJMD8>
2025-05-05 07:45:51 -0400

    Ajoutez des fichiers projet.

---
+++ Northwind2/Program.cs
@@ -0,0 +1,54 @@
+
+using Microsoft.EntityFrameworkCore;
+using Northwind2.Data;
+
+namespace Northwind2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            // Add services to the container.
+            // enregistre la classe de contexte de donnees comme service en lui indiquant la chaine de connexion a utiliser
+            string? connect = builder.Configuration.GetConnectionString("Northwind2Connect");
+            builder.Services.AddDbContext<ContexteNorthwind>(options =>
+                options.UseSqlServer(connect));
+
+
+
+
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            //builder.Services.AddOpenApi();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                //app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
